afd181f693f87d6ee6d159e88c8805eed7d19031
Wayne Delport <Wayne.Delport@standardbank.co.za>
2018-12-20 11:06:01 +0200

    Add Single Responsibility Principle Overview playground page and remove overview comments from the playground files

--- 02 Solid-Single-Responsibility-Principle-Good.playground/Contents.swift
+++ 02 Solid-Single-Responsibility-Principle-Good.playground/Contents.swift
@@ -1,19 +1,3 @@
-/*
- Single Responsibility Principle
- 
- 1. Meaning
- Class should have only one responsibility which means class should be highly cohesive and implement strongly related logic. Class implementing feature 1 AND feature 2 AND feature 3 (and so on) violates SRP.
- 
- 2. How to recognize code smell?
- 2.1 More than one contextually separated piece of code within single class.
- 2.2 Large setup in tests (TDD is very useful when it comes to detecting SRP violation).
- 
- 3. Benefits
- 3.1 Separated classes responsible for given use case can be now reused in other parts of an application.
- 3.2. Separated classes responsible for given use case can be now tested separately.
- 
- */
-
 /* GOOD */
 
 import UIKit
---
+++ Overview.playground/Pages/Overview.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+/*:
+ 
+# Single Responsibility Principle
+- - - - - - - - - -
+
+## 1. Meaning
+- Class should have only one responsibility which means class should be highly cohesive and implement strongly related logic. Class implementing feature 1 AND feature 2 AND feature 3 (and so on) violates SRP.
+
+ ---
+
+ ## 2. How to recognize code smell?
+- More than one contextually separated piece of code within single class
+- Large setup in tests (TDD is very useful when it comes to detecting SRP violation)
+
+ ---
+
+ ## 3. Benefits
+- Separated classes responsible for given use case can be now reused in other parts of an application
+- Separated classes responsible for given use case can be now tested separately
+ 
+*/
